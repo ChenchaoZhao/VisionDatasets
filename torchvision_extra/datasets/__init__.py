ea210ee0c3e75f06bae9aa895b59b702a65093ab
Chenchao Zhao <35147961+ChenchaoZhao@users.noreply.github.com>
2022-01-28 12:37:11 +0800

    typo

--- torchvision_extra/datasets/__init__.py
+++ torchvision_extra/datasets/__init__.py
@@ -1,4 +1,4 @@
 from torchvision.datasets import *
 from torchvision.datasets.vision import StandardTransform
 
-from .oxford_iit_pet import OxfordIIITPet
+from .oxford_iiit_pet import OxfordIIITPet
