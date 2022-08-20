2db544c8db9ac1ad213ab3d054d3e6b21bd0d8ba
Chenchao Zhao <35147961+ChenchaoZhao@users.noreply.github.com>
2022-08-20 16:32:41 -0400

    refactor

--- torchvision_extra/datasets/__init__.py
+++ torchvision_extra/datasets/__init__.py
@@ -1,6 +1,5 @@
 from torchvision.datasets import *
 from torchvision.datasets.vision import StandardTransform
 
-from .oxford_iiit_cat_dog import OxfordCatDog
-from .oxford_iiit_pet import OxfordIIITPet
+from .oxford_iiit_pet_plus import OxfordIIITPetPlus
 from .stanford_dogs import StanfordDogs
