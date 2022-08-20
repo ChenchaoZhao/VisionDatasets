bf2bed7979d0e7822fd2310483de4fe97712a768
Chenchao Zhao <35147961+ChenchaoZhao@users.noreply.github.com>
2022-08-20 16:31:50 -0400

    update

--- torchvision_extra/datasets/oxford_iiit_cat_dog.py
+++ torchvision_extra/datasets/oxford_iiit_cat_dog.py
@@ -1,57 +1,71 @@
-from typing import Any, Callable, Dict, Optional, Sequence, Union
+from typing import Any, Callable, Dict, Optional, Sequence, Tuple, Union
 
-from .oxford_iiit_pet import OxfordIIITPet
+import numpy as np
+import PIL
+from torchvision.datasets import OxfordIIITPet
 
-__all__ = ["OxfordCatDog"]
+__all__ = ["OxfordIIITPetPlus"]
 
 
-class OxfordCatDog(OxfordIIITPet):
-    """Oxford IIIT Cat and Dog Dataset."""
+class OxfordIIITPetPlus(OxfordIIITPet):
+    """`Oxford-IIIT Pet Dataset   <https://www.robots.ox.ac.uk/~vgg/data/pets/>`_.
+    Args:
+        root (string): Root directory of the dataset.
+        split (string, optional): The dataset split, supports ``"trainval"`` (default) or ``"test"``.
+        target_types (string, sequence of strings, optional): Types of target to use. Can be ``category`` (default) or
+            ``segmentation``. Can also be a list to output a tuple with all specified target types. The types represent:
+                - ``category`` (int): Label for one of the 37 pet categories.
+                - ``coarse_category`` (int): Label for cat or dog
+                - ``segmentation`` (PIL image): Segmentation trimap of the image.
+                - ``detection``: Bounding box around segmentation mask
+            If empty, ``None`` will be returned as target.
+        transform (callable, optional): A function/transform that takes in a PIL image and returns a transformed
+            version. E.g, ``transforms.RandomCrop``.
+        target_transform (callable, optional): A function/transform that takes in the target and transforms it.
+        download (bool, optional): If True, downloads the dataset from the internet and puts it into
+            ``root/oxford-iiit-pet``. If dataset is already downloaded, it is not downloaded again.
+    """
 
     def __init__(
         self,
         root: str,
         split: str = "trainval",
         target_types: Union[Sequence[str], str] = "category",
-        return_bbox: bool = False,
         transforms: Optional[Callable] = None,
         transform: Optional[Callable] = None,
         target_transform: Optional[Callable] = None,
         download: bool = False,
-    ) -> None:
-
-        if return_bbox and "segmentation" not in target_types:
-            if isinstance(target_types, str):
-                target_types = (
-                    target_types,
-                    "segmentation",
-                )
-            else:  # is a sequence e.g. ('category',)
-                target_types = list(target_types)
-                target_types.append("segmentation")
+    ):
 
         super().__init__(
             root,
             split,
-            target_types,
+            ("category", "segmentation"),
             transforms,
             transform,
             target_transform,
             download,
         )
 
-        self.return_bbox = return_bbox
         self.read_me = self._load_readme()
         # load species and breed labels
         self._load_labels()
+        self._compute_boxes()
+        self.target_types = target_types
+
+    def _compute_boxes(self):
+        self._boxes = []
+        for idx in range(len(self)):
+            mask = PIL.Image.open(self._segs[idx])
+            ydx, xdx = np.where(np.array(mask) == 3)
+            box = [[xdx.min(), ydx.min(), xdx.max(), ydx.max()]]
+            self._boxes.append(box)
 
     def _load_readme(self) -> str:
         with open(self._anns_folder / "README", "r") as f:
             return f.read()
 
     def _load_labels(self) -> None:
-
-        pass
 
         self.class_to_idx_tuple = {}
         self.idx_to_name = {}
@@ -100,23 +114,28 @@ class OxfordCatDog(OxfordIIITPet):
             else:  # species == 'Dog'
                 self.dog_breed_to_breed_idx[breed] = idx_tuple[-1]
 
-    def __getitem__(self, idx: int) -> Dict[str, Any]:
-        image, target = super().__getitem__(idx)
-        outputs = dict(image=image)
+    def __getitem__(self, idx: int) -> Tuple[Any, Dict[str, Any]]:
+        image = PIL.Image.open(self._images[idx]).convert("RGB")
 
-        if target is None:
-            return outputs
+        target: Dict[str, Any] = {}
+        for target_type in self._target_types:
+            if target_type == "category":
+                target["flat_label"] = self._labels[idx]
+            elif target_type == "coarse_category":
+                species_label, breed_label = self.idx_to_species_breed_idx[
+                    self._labels[idx]
+                ]
+                target["flat_label"] = self._labels[idx]
+                target["species_label"] = species_label
+                target["breed_label"] = breed_label
+            elif target_type == "detection":
+                target["boxes"] = self._boxes[idx]
+            elif target_type == "segmentation":
+                target.append(PIL.Image.open(self._segs[idx]))
+            else:
+                raise KeyError(f"Target type ({target_type}) not found.")
 
-        if len(self._target_types) == 1:
-            target = [target]
+        if self.transforms:
+            image, target = self.transforms(image, target)
 
-        for tdx, target_type in enumerate(self._target_types):
-            outputs[target_type] = target[tdx]
-
-        flat_label = self._labels[idx]
-        species_label, breed_label = self.idx_to_species_breed_idx[flat_label]
-        outputs["species"] = species_label
-        outputs["breed"] = breed_label
-        outputs["flat_label"] = flat_label
-
-        return outputs
+        return image, target
